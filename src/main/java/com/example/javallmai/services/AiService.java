24cd69ca91408dfd0a7c6009374ae93e79b2a2d4
David Uebelacker <david@uebelacker.ch>
2023-10-25 21:21:20 +0200

    added yoda system role

--- src/main/java/com/example/javallmai/services/AiService.java
+++ src/main/java/com/example/javallmai/services/AiService.java
@@ -5,8 +5,12 @@ import com.example.javallmai.models.Message;
 import org.springframework.ai.client.AiClient;
 import org.springframework.ai.client.AiResponse;
 import org.springframework.ai.prompt.Prompt;
+import org.springframework.ai.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,12 +18,22 @@ public class AiService {
 
     private final AiClient aiClient;
 
+    @Value("classpath:/prompts/system.st")
+    private Resource systemResource;
+
     public AiService(AiClient aiClient) {
         this.aiClient = aiClient;
     }
 
     public Message generate(List<Message> messages) {
-        Prompt prompt = new Prompt(MessageMapper.map(messages));
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemResource);
+
+        List<org.springframework.ai.prompt.messages.Message> mappedMessages = new ArrayList<>();
+        mappedMessages.add(systemPromptTemplate.createMessage());
+        mappedMessages.addAll(MessageMapper.map(messages));
+
+        Prompt prompt = new Prompt(mappedMessages);
+
         AiResponse response = aiClient.generate(prompt);
         return MessageMapper.map(response.getGeneration());
     }
